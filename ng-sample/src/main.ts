29d4dc8a0c5d749e3e60c2a66b15d7243dfb0ce4
Jawad Shani <jawad.shani@hitachi-eu.com>
2023-09-15 00:25:15 +0200

    basic app

--- ng-sample/src/main.ts
+++ ng-sample/src/main.ts
@@ -2,6 +2,24 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import { onCLS, onFID, onLCP, onFCP, onINP, onTTFB } from 'web-vitals';
+
+
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+  onCLS(sendToAnalytics);
+  onFID(sendToAnalytics);
+  onLCP(sendToAnalytics);
+  onFCP(sendToAnalytics);
+  onINP(sendToAnalytics);
+  onTTFB(sendToAnalytics);
+  function sendToAnalytics(metric: object) {
+      console.log(metric);
+      const body = JSON.stringify(metric);
+
+      (navigator.sendBeacon && navigator.sendBeacon('/analytics', body)) ||
+          fetch('/analytics', { body, method: 'POST', keepalive: true });
+
+  }
